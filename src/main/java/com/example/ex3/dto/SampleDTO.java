6f38ca4b80b1a333de8b698b1c251937095b7e0c
wpdls2821@gmail.com <wpdls2821@gmail.com>
2023-04-04 16:06:22 +0900

    Initial commit

---
+++ src/main/java/com/example/ex3/dto/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+import b
+import java.time.LocalDateTime;
+
+@Data
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
+}
