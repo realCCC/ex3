654d2d67a97be2cb83892bd54a15f3a39b8d0755
wpdls2821@gmail.com <wpdls2821@gmail.com>
2023-04-11 14:59:49 +0900

    0411

--- src/main/java/com/example/ex3/controller/SampleController.java
+++ src/main/java/com/example/ex3/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -39,4 +40,20 @@ public class SampleController {
         model.addAttribute("list",list);
     }
 
+    @GetMapping("exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First...100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "Success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+        return "redirect:/sample/ex3";
+    }
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3() 호출됨");
+    }
 }
